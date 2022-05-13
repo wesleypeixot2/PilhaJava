f99c05bc4c45721dc125664d2d23e44d230f4391
Wesley <leon.peixoto2@hotmail.com>
2022-05-12 21:38:51 -0300

    alteração 3

--- src/com/company/LabPrincipal.java
+++ src/com/company/LabPrincipal.java
@@ -41,56 +41,33 @@ public class LabPrincipal {
 
     //Criando o esqueleto do Labirinto
     public String[][] esqueletoLabirinto(){
-        String [][] tracoLabirinto = new String[11][11];
-        for (int lin = 0; lin < tracoLabirinto.length; lin++) {
-            for (int col = 0; col < tracoLabirinto.length; col++) {
-                tracoLabirinto[lin][col] = "#";
-        }
-     }
-     return caminhoLabirinto(tracoLabirinto);
-    }
-
-    //Corpo do labirinto
-    public String[][] caminhoLabirinto(String[][] labirinto) {
-        int col = 1;
-        setIdxCol(col);
-        int lin = 1;
-        setIdxLin(lin);
-        for (int i = 1; i < 10; i++) {
-            labirinto[i][1] = " ";
-            labirinto[9][i] = " ";
-            labirinto[i][9] = " ";
-            labirinto[5][i] = " ";
-            labirinto[7][i] = " ";
-            labirinto[3][i] = " ";
-            labirinto[1][i] = " ";
-            labirinto[6][3] = " ";
-            labirinto[2][7] = " ";
-            labirinto[2][5] = " ";
-            labirinto[4][5] = " ";
-            labirinto[5][2] = "#";
-            labirinto[7][2] = "#";
-            labirinto[5][8] = "#";
-            labirinto[3][3] = "#";
-            labirinto[3][6] = "#";
-            labirinto[1][2] = "#";
-            labirinto[1][6] = "#";
-            labirinto[1][8] = "#";
-        }
-        labirinto[col][lin] = "E";
-        labirinto[1][3] = "S";
-        return labirinto;
+        setIdxCol(3);
+        setIdxLin(2);
+        String [][] labirinto = {
+                {"0","1","2","3","4","5","6","7","8","9","0","1","2","3"},
+                {"1","|","#","#","#","#","#","#","#","#","#","#","#","|"},
+                {"2","|","#","E","#","S"," "," ","#"," ","#"," ","#","|"},
+                {"3","|","#"," ","#","#","#"," ","#"," ","#"," ","#","|"},
+                {"4","|","#"," "," ","#"," "," ","#"," "," "," ","#","|"},
+                {"5","|","#"," ","#","#","#"," ","#","#","#"," ","#","|"},
+                {"6","|","#"," ","#"," "," "," "," "," ","#"," ","#","|"},
+                {"7","|","#"," ","#"," ","#","#","#","#","#"," ","#","|"},
+                {"8","|","#"," ","#"," "," "," "," "," "," "," ","#","|"},
+                {"9","|","#"," ","#","#","#","#","#","#","#"," ","#","|"},
+                {"0","|","#"," "," "," "," "," "," "," "," "," ","#","|"},
+                {"1","|","#","#","#","#","#","#","#","#","#","#","#","|"}
+        };
+     return labirinto;
     }
 
     //Numeração e ordenação da quantidade de linhas e colunas do labirinto
     public void linhaColunaLabirinto(String[][] labirinto){
-        System.out.println("0 1 2 3 4 5 6 7 8 9 10 11 12");
-        for (int coluna = 0; coluna < labirinto.length; coluna++) {
-            System.out.print((coluna != 10 ? coluna + " | " : "0 | "));
-            for (int linha = 0; linha < labirinto.length; linha++) {
+        System.out.println(labirinto.length);
+        for (int coluna = 0; coluna < 12; coluna++) {
+            System.out.println();
+            for (int linha = 0; linha < 14; linha++) {
                 System.out.print(labirinto[coluna][linha] + " ");
             }
-            System.out.println("|");
         }
         System.out.println();
     }
